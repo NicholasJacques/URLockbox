ad70918389aecbf1d7ace1330d59f44c48072a01
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-07-17 14:37:45 -0600

    completes sessions controller test

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -8,4 +8,39 @@ RSpec.describe SessionsController do
       expect(response).to render_template(:new)
     end
   end
+
+  describe 'POST#create' do
+    context 'with valid credentials' do
+      it 'it sets session[:user_id] to logged in user' do
+        user = create(:user)
+        
+        post :create, params: { session: { email: user.email, password: "password" } }
+
+        expect(assigns[:user]).to eq(user)
+        expect(session[:user_id]).to eq(user.id)
+        expect(controller).to set_flash[:success]
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context 'with invalid credentials' do
+      it 'flashes failure message and redirects to login' do
+        post :create, params: { session: { email: 'test@test.com', password: 'password' } }
+
+        expect(controller).to set_flash[:failure]
+        expect(response).to redirect_to(new_session_path)
+      end
+    end
+  end
+
+  describe 'DELETE#destroy' do
+    it 'clears session and redirects to log in' do
+      user = create(:user)
+      session[:user_id] = user.id
+
+      delete :destroy, params: {id: user.id}
+
+      expect(session[:user_id]).to be(nil)
+    end
+  end
 end
