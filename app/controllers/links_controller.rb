7105a6fb60583e0105728898ad14d14c49192fd9
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-07-27 12:34:16 -0600

    user can edit link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,4 +3,21 @@ class LinksController < ApplicationController
 
   def index
   end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    link = Link.find(params[:id])
+    if link.update_attributes(link_params)
+      redirect_to links_path
+    end
+  end
+  
+  private
+
+  def link_params
+    params.require(:link).permit(:url, :title)
+  end
 end
